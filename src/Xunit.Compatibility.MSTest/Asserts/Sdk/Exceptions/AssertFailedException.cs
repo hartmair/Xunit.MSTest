439ace76538dc3c9a9773becd79cfcb804e8eece
Markus Hartmair <hartmair@yahoo.de>
2020-07-26 08:22:23 +0200

    Fix package dependencies

--- src/Xunit.Compatibility.MSTest/Asserts/Sdk/Exceptions/AssertFailedException.cs
+++ src/Xunit.Compatibility.MSTest/Asserts/Sdk/Exceptions/AssertFailedException.cs
@@ -1,9 +1,28 @@
-﻿namespace Microsoft.VisualStudio.TestTools.UnitTesting
+﻿using System;
+
+namespace Microsoft.VisualStudio.TestTools.UnitTesting
 {
-    public class AssertFailedException : Xunit.Sdk.XunitException
+    public class AssertFailedException : Exception
     {
         public AssertFailedException(string message) : base(message)
         {
+        }
+
+        // see https://github.com/xunit/assert.xunit/blob/main/Sdk/Exceptions/XunitException.cs
+        /// <inheritdoc/>
+        public override string ToString()
+        {
+            var result = GetType().ToString();
+            if (!string.IsNullOrEmpty(Message))
+            {
+                result += $": {Message}";
+            }
+            if (StackTrace != null)
+            {
+                result += Environment.NewLine + StackTrace;
+            }
+            return result;
+
         }
     }
 }
